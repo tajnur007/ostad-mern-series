0acf12a5d866eb786444584349c76b0feed84dbb
Kazi Tajnur Islam <tajnur007@gmail.com>
2023-12-21 00:01:08 +0600

    MongoDB database connection established for task-tracker

--- batch-4/task-tracker/backend/app.js
+++ batch-4/task-tracker/backend/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-// const router = require('./src/routes/api');
-const app = new express();
-
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const path = require("path");
-// const mongoose = require('mongoose');
-// const mongoSanitize = require('express-mongo-sanitize');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// const router = require('./src/routes/api');
+
+
+
+// Initializing express application
+const app = new express();
 
 // Using rate limit middleware
 const limiter = rateLimit({
@@ -34,35 +38,23 @@ app.use(hpp());
 // Using cors for enabling CORS 
 app.use(cors());
 
+// Using cookie parser for managing cookies
+app.use(cookieParser());
+
+// Using MongoDB sanitize for sanitizing user input
+app.use(mongoSanitize());
+
+// MongoDB Connection
+async function main() {
+  const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.hwr1u.mongodb.net/`;
+
+  await mongoose.connect(connectionString);
+
+  console.log('---MongoDB Database Connected---');
+}
+main().catch(err => console.log('Error occured::', err));
 
 
-// let URL = "mongodb+srv://<username>:<password>@cluster0.7uslu.mongodb.net/ecom4?retryWrites=true&w=majority";
-// let option = { user: 'testuser7777', pass: "testuser7777", autoIndex: true };
-// mongoose.connect(URL, option).then((res) => {
-//   console.log("Database Connected")
-// }).catch((err) => {
-//   console.log(err)
-// })
-
-// app.use(cookieParser());
-// app.use(cors())
-// app.use(helmet())
-// app.use(mongoSanitize())
-// app.use(xss())
-// app.use(hpp())
-
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb' }));
-
-
-// const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 })
-// app.use(limiter)
-
-
-// app.use("/api/v1", router)
-app.get('/products', function (req, res) {
-  res.json({ msg: 'This is CORS-disabled for all origins!' })
-});
 
 
 module.exports = app;
